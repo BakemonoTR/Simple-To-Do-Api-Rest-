d4af81dbe94190d9f350f1043714b9304a96221a
unknown <envercanbickin@gmail.com>
2025-11-03 17:35:40 +0300

    First version

---
+++ main.py
@@ -0,0 +1,104 @@
+from fastapi import FastAPI, HTTPException, Depends
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Optional, List, AsyncGenerator
+from contextlib import asynccontextmanager
+from pathlib import Path
+
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
+g
+
+class TaskCreate(SQLModel):
+    title: str
+    description: Optional[str] = None
+
+
+class TaskUpdate(SQLModel):
+    title: Optional[str]
+    description: Optional[str] = None
+    completed: Optional[bool] = None
+
+
+BASE_DIR = Path(__file__).resolve().parent
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{BASE_DIR / sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True, connect_args={"check_same_thread": False})
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/tasks", response_model=Task, status_code=201)
+def create_task(task_in: TaskCreate, session: Session = Depends(get_session)):
+    db_task = Task(**task_in.dict(), completed=False)
+
+    session.add(db_task)
+    session.commit()
+
+    session.refresh(db_task)
+
+    return db_task
+
+
+@app.get("/tasks", response_model=List[Task])
+def get_tasks(session: Session = Depends(get_session)):
+    statement = select(Task)
+    tasks = session.exec(statement).all()
+    return tasks
+
+
+@app.get("/tasks/{task_id}", response_model=Task)
+def get_task(task_id: int, session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, task_in: TaskUpdate, session: Session = Depends(get_session)):
+    db_task = session.get(Task, task_id)
+    if not db_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    task_data = task_in.dict(exclude_unset=True)
+
+    for key, value, in task_data.items():
+        setattr(db_task, key, value)
+
+    session.add(db_task)
+    session.commit()
+    session.refresh(db_task)
+    return db_task
+
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int, session: Session = Depends(get_session)):
+    db_task = session.get(Task, task_id)
+    if not db_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    session.delete(db_task)
+    session.commit()
+
+    return
